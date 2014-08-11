0726d6c260d4d3976c22dc5be66d2a24d35ce3ad
0xAX <testtest.89@mail.ru>
2014-08-11 19:33:20 +0600

    indentation fixed

--- collections/queue/queue.go
+++ collections/queue/queue.go
@@ -1,26 +1,26 @@
 package queue
 
 type Queue struct {
-	queue []interface{}
+    queue []interface{}
     len int
 }
 
 func New() *Queue {
-	queue := &Queue{}
-	queue.queue = make([]interface{}, 0)
+    queue := &Queue{}
+    queue.queue = make([]interface{}, 0)
     queue.len = 0
     return queue
 }
 
 func (queue *Queue) Length() int {
-	return queue.len
+    return queue.len
 }
 
 func (q *Queue) Remove() interface{} {
-	tmp := q.queue[0]
-	q.queue = q.queue[1:]
-	q.len -= 1
-	return tmp
+    tmp := q.queue[0]
+    q.queue = q.queue[1:]
+    q.len -= 1
+    return tmp
 }
 
 func (q *Queue) Peek() interface{} {
@@ -28,6 +28,6 @@ func (q *Queue) Peek() interface{} {
 }
 
 func (q *Queue) Add(value interface{}) {
-	q.len += 1
-	q.queue = append(q.queue, value)
+    q.len += 1
+    q.queue = append(q.queue, value)
 } 
