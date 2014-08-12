f7794af38d6740bda661fa3b7db4c86ac9b80b89
0xAX <testtest.89@mail.ru>
2014-08-12 23:35:29 +0600

    indentation fixed

--- algorithms/number/gcd.go
+++ algorithms/number/gcd.go
@@ -1,47 +1,47 @@
 package gcd
 
 func gcd(x uint, y uint) uint {
-	
-	var shift uint = 0
+    
+    var shift uint = 0
 
-	if x == y {
-		return x
-	}
+    if x == y {
+        return x
+    }
 
-	if x == 0 {
-		return y
-	}
+    if x == 0 {
+        return y
+    }
 
-	if y == 0 {
-		return x
-	}
+    if y == 0 {
+        return x
+    }
 
-	for shift := 0; (x | y) & 1 == 0; shift++ {
-		x = x >> 1
-		y = y >> 1
-	}
+    for shift := 0; (x | y) & 1 == 0; shift++ {
+        x = x >> 1
+        y = y >> 1
+    }
 
-	for ; (x & 1) == 0 ; {
-		x = x >> 1
-	}
+    for ; (x & 1) == 0 ; {
+        x = x >> 1
+    }
 
-	for ; y == 0 ; {
-		
-		for ; (y & 1) == 0 ; {
-			y = y >> 1
-		}
+    for ; y == 0 ; {
+        
+        for ; (y & 1) == 0 ; {
+            y = y >> 1
+        }
 
-		if x > y {
-			t := x
-			x = y
-			y = t
-		}
+        if x > y {
+            t := x
+            x = y
+            y = t
+        }
 
-		y = y - x
+        y = y - x
 
-	}
+    }
 
     y = y << shift
 
-	return y
+    return y
 }
