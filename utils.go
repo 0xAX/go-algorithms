3a1d5c9dfcebc7334e2cdd4553694c5341c77927
0xAX <0xAX@users.noreply.github.com>
2015-12-26 18:34:44 +0600

    Update utils.go

--- utils.go
+++ utils.go
@@ -4,10 +4,8 @@ import "math/rand"
 
 func RandArray(n int) []int {
     arr := make([]int, n)
-    
     for i := 0; i <= n - 1; i++ {
         arr[i] = rand.Intn(n)
     }
-    
     return arr
 }
