17585bda6fc127fc30946718d7fb6666a184a1bf
srksumanth <srksumanth@gmail.com>
2018-01-24 15:10:41 +0530

    remove extra iteration

--- sorting/bubble_sort.go
+++ sorting/bubble_sort.go
@@ -9,21 +9,21 @@ import "fmt"
 import "github.com/0xAX/go-algorithms"
 
 func main() {
-    arr := utils.RandArray(10)
-    fmt.Println("Initial array is:", arr)
-    fmt.Println("")
-    
-    tmp := 0
-    
-    for i := 0; i < len(arr); i++ {
-        for j := 0; j < len(arr) - 1; j++ {
-            if arr[j] > arr[j + 1] {
-                tmp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = tmp
-            }
-        }
-    }
-    
-    fmt.Println("Sorted array is: ", arr)
+	arr := utils.RandArray(10)
+	fmt.Println("Initial array is:", arr)
+	fmt.Println("")
+
+	tmp := 0
+
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				tmp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = tmp
+			}
+		}
+	}
+
+	fmt.Println("Sorted array is: ", arr)
 }
