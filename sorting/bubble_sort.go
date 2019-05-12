7a3ac778669bb2beeecd82ebf554bf2e784c769c
Ansil H <ansilh@gmail.com>
2019-05-13 00:23:24 +0530

    Added time package Removed temporary variable from bubble sort

--- sorting/bubble_sort.go
+++ sorting/bubble_sort.go
@@ -13,14 +13,10 @@ func main() {
 	fmt.Println("Initial array is:", arr)
 	fmt.Println("")
 
-	tmp := 0
-
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr)-1-i; j++ {
 			if arr[j] > arr[j+1] {
-				tmp = arr[j]
-				arr[j] = arr[j+1]
-				arr[j+1] = tmp
+				arr[j],arr[j+1] = arr[j+1],arr[j]
 			}
 		}
 	}
--- utils.go
+++ utils.go
@@ -1,14 +1,17 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func RandArray(n int) []int {
-    // needed a seed input else it will generate the same number
-    rand.Seed(time.Now().UnixNano())
-    
-    arr := make([]int, n)
-    for i := 0; i <= n - 1; i++ {
-        arr[i] = rand.Intn(n)
-    }
-    return arr
+	// needed a seed input else it will generate the same number
+	rand.Seed(time.Now().UnixNano())
+
+	arr := make([]int, n)
+	for i := 0; i <= n-1; i++ {
+		arr[i] = rand.Intn(n)
+	}
+	return arr
 }
